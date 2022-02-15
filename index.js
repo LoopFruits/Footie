dbcbd0538d25d63ec95a6e13f637880c1ae8994f
LoopFruits <Kensworldc@gmail.com>
2022-02-15 10:41:52 -0500

    js sudo code layout

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
+fetch("https://api-football-beta.p.rapidapi.com/fixtures/events?fixture=37899", {// fixture events 
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "api-football-beta.p.rapidapi.com",
+		"x-rapidapi-key": "650869111emshf0716b578faa71fp19f0e6jsn45cd4d8740f6"
+	}
+}) 
+.then(response => response.json())
+.then(fixtureData => console.log(fixtureData))
+
+
+
+// team one image 
+
+
+// event listener for team one button which will display the image of the team 
+
+
+
+//display team 1 stats 
+
+
+
+//team two image 
+
+
+// event listener for team two button which will display the image of the team 
+
+
+// display team 2 stats 
+
+
+
+// random match generator for the dice image/button 
+
+
+// event listener for the dice
+
+
+
+//match stats? 
+
+
+
+
+//prediction for who will win in separate container 
+
+
+
+
+
+
+
+
+// display  in container for results (who won (1-0))
+
+
+
 
